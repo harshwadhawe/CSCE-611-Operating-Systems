2555088196fb6c394661bca3829c4e27b3e1a552
harshwadhawe <harsh.wadhawe@tamu.edu>
2025-12-11 20:26:35 -0600

    Complete MP7

--- MP7/MP7_Sources/file.C
+++ MP7/MP7_Sources/file.C
@@ -28,16 +28,29 @@
 
 File::File(FileSystem *_fs, int _id) {
     Console::puts("Opening file.\n");
-    Console::puts("FUNCTION NOT IMPLEMENTED\n");
-    assert(false);
+    fs = _fs;
+    inode = fs -> LookupFile(_id);
+    current_position = 0;
+    cached_block_idx = (unsigned int)-1; // No block cached yet
+    
+    // Initialize cache to zeros (will be loaded on demand during read/write)
+    for (unsigned int i = 0; i < SimpleDisk::BLOCK_SIZE; i++) {
+        block_cache[i] = 0;
+    }
 }
 
 File::~File() {
     Console::puts("Closing file.\n");
-    /* Make sure that you write any cached data to disk. */
-    /* Also make sure that the inode in the inode list is updated. */
-    Console::puts("FUNCTION NOT IMPLEMENTED\n");
-    assert(false);
+    if (inode != nullptr && inode -> block_numbers_block != 0 && cached_block_idx != (unsigned int)-1) {
+        // Write current cache block if we have a valid cached block
+        unsigned int* block_nums = new unsigned int[SimpleDisk::BLOCK_SIZE / sizeof(unsigned int)];
+        fs -> disk -> read(inode -> block_numbers_block, (unsigned char*)block_nums);
+        if (cached_block_idx < inode -> num_blocks && block_nums[cached_block_idx] != 0) {
+            fs -> disk -> write(block_nums[cached_block_idx], block_cache);
+        }
+        delete[] block_nums;
+        fs -> SaveInodes();
+    }
 }
 
 /*--------------------------------------------------------------------------*/
@@ -46,24 +59,146 @@ File::~File() {
 
 int File::Read(unsigned int _n, char *_buf) {
     Console::puts("reading from file\n");
-    Console::puts("FUNCTION NOT IMPLEMENTED\n");
-    assert(false);
+    if (inode == nullptr || inode -> block_numbers_block == 0) {
+        return 0;
+    }
+    
+    unsigned int available = inode -> file_length - current_position;
+    unsigned int to_read = (_n < available) ? _n : available;
+    unsigned int bytes_read = 0;
+    
+    // Load block numbers
+    unsigned int* block_nums = new unsigned int[SimpleDisk::BLOCK_SIZE / sizeof(unsigned int)];
+    fs -> disk -> read(inode -> block_numbers_block, (unsigned char*)block_nums);
+    
+    while (bytes_read < to_read) {
+        unsigned int block_idx = (current_position + bytes_read) / SimpleDisk::BLOCK_SIZE;
+        unsigned int offset_in_block = (current_position + bytes_read) % SimpleDisk::BLOCK_SIZE;
+        
+        if (block_idx >= inode -> num_blocks || block_nums[block_idx] == 0) {
+            break; // No more data
+        }
+        
+        // Load the block if not already cached
+        if (block_idx != cached_block_idx) {
+            if (block_nums[block_idx] != 0) {
+                fs -> disk -> read(block_nums[block_idx], block_cache);
+            } else {
+                for (unsigned int i = 0; i < SimpleDisk::BLOCK_SIZE; i++) {
+                    block_cache[i] = 0;
+                }
+            }
+            cached_block_idx = block_idx;
+        }
+        
+        // Copy from cache
+        unsigned int remaining_in_block = SimpleDisk::BLOCK_SIZE - offset_in_block;
+        unsigned int remaining_to_read = to_read - bytes_read;
+        unsigned int copy_count = (remaining_in_block < remaining_to_read) ? remaining_in_block : remaining_to_read;
+        
+        for (unsigned int i = 0; i < copy_count; i++) {
+            _buf[bytes_read + i] = block_cache[offset_in_block + i];
+        }
+        
+        bytes_read += copy_count;
+    }
+    
+    current_position += bytes_read;
+    delete[] block_nums;
+    return bytes_read;
 }
 
 int File::Write(unsigned int _n, const char *_buf) {
     Console::puts("writing to file\n");
-    Console::puts("FUNCTION NOT IMPLEMENTED\n");
-    assert(false);
+    if (inode == nullptr || inode -> block_numbers_block == 0) {
+        return 0;
+    }
+    
+    unsigned int max_file_size = Inode::MAX_BLOCKS * SimpleDisk::BLOCK_SIZE;
+    unsigned int max_write = max_file_size - current_position;
+    unsigned int to_write = (_n < max_write) ? _n : max_write;
+    unsigned int bytes_written = 0;
+    
+    // Load block numbers
+    unsigned int* block_nums = new unsigned int[SimpleDisk::BLOCK_SIZE / sizeof(unsigned int)];
+    fs -> disk -> read(inode -> block_numbers_block, (unsigned char*)block_nums);
+    
+    while (bytes_written < to_write) {
+        unsigned int block_idx = (current_position + bytes_written) / SimpleDisk::BLOCK_SIZE;
+        unsigned int offset_in_block = (current_position + bytes_written) % SimpleDisk::BLOCK_SIZE;
+        
+        // Allocate new block if needed
+        if (block_idx >= inode -> num_blocks) {
+            if (inode -> num_blocks >= Inode::MAX_BLOCKS) {
+                break; // Maximum file size reached
+            }
+            
+            int new_block = fs -> GetFreeBlock();
+            if (new_block == -1) {
+                break; // No free blocks available
+            }
+            
+            block_nums[inode -> num_blocks] = new_block;
+            fs -> free_blocks[new_block] = 1;
+            inode -> num_blocks++;
+            
+            // Initialize new block to zeros
+            for (unsigned int i = 0; i < SimpleDisk::BLOCK_SIZE; i++) {
+                block_cache[i] = 0;
+            }
+            fs -> disk -> write(new_block, block_cache);
+            
+            // Save updated block numbers
+            fs -> disk -> write(inode -> block_numbers_block, (unsigned char*)block_nums);
+            fs -> SaveFreeList();
+        }
+        
+        // Load the block if not already cached
+        if (block_idx != cached_block_idx) {
+            if (block_nums[block_idx] != 0) {
+                fs -> disk -> read(block_nums[block_idx], block_cache);
+            } else {
+                for (unsigned int i = 0; i < SimpleDisk::BLOCK_SIZE; i++) {
+                    block_cache[i] = 0;
+                }
+            }
+            cached_block_idx = block_idx;
+        }
+        
+        // Copy to cache
+        unsigned int remaining_in_block = SimpleDisk::BLOCK_SIZE - offset_in_block;
+        unsigned int remaining_to_write = to_write - bytes_written;
+        unsigned int copy_count = (remaining_in_block < remaining_to_write) ? remaining_in_block : remaining_to_write;
+        
+        for (unsigned int i = 0; i < copy_count; i++) {
+            block_cache[offset_in_block + i] = _buf[bytes_written + i];
+        }
+        
+        // Write block back to disk
+        fs -> disk -> write(block_nums[block_idx], block_cache);
+        
+        bytes_written += copy_count;
+    }
+    
+    current_position += bytes_written;
+    if (current_position > inode -> file_length) {
+        inode -> file_length = current_position;
+    }
+    
+    delete[] block_nums;
+    return bytes_written;
 }
 
 void File::Reset() {
     Console::puts("resetting file\n");
-    Console::puts("FUNCTION NOT IMPLEMENTED\n");
-    assert(false);
+    current_position = 0;
+    cached_block_idx = (unsigned int)-1; // Invalidate cache
 }
 
 bool File::EoF() {
     Console::puts("checking for EoF\n");
-    Console::puts("FUNCTION NOT IMPLEMENTED\n");
-    assert(false);
+    if (inode == nullptr) {
+        return true;
+    }
+    return current_position >= inode -> file_length;
 }
--- MP7/MP7_Sources/file_system.C
+++ MP7/MP7_Sources/file_system.C
@@ -38,16 +38,23 @@
 /*--------------------------------------------------------------------------*/
 
 FileSystem::FileSystem() {
-    Console::puts("In file system constructor.\n");
-    Console::puts("FUNCTION NOT IMPLEMENTED\n");
-    assert(false);
+    disk = nullptr;
+    size = 0;
+    inodes = nullptr;
+    free_blocks = nullptr;
 }
 
 FileSystem::~FileSystem() {
-    Console::puts("unmounting file system\n");
-    /* Make sure that the inode list and the free list are saved. */
-    Console::puts("FUNCTION NOT IMPLEMENTED\n");
-    assert(false);
+    if (disk != nullptr) {
+        SaveInodes();
+        SaveFreeList();
+    }
+    if (inodes != nullptr) {
+        delete[] inodes;
+    }
+    if (free_blocks != nullptr) {
+        delete[] free_blocks;
+    }
 }
 
 
@@ -58,41 +65,171 @@ FileSystem::~FileSystem() {
 
 bool FileSystem::Mount(SimpleDisk * _disk) {
     Console::puts("mounting file system from disk\n");
-    /* Here you read the inode list and the free list into memory */
-    Console::puts("FUNCTION NOT IMPLEMENTED\n");
-    assert(false);
+    disk = _disk;
+    size = disk -> NaiveSize();
+    unsigned int num_blocks = size / SimpleDisk::BLOCK_SIZE;
+    
+    inodes = new Inode[MAX_INODES];
+    free_blocks = new unsigned char[num_blocks];
+    
+    disk -> read(0, (unsigned char*)inodes);
+    disk -> read(1, free_blocks);
+    
+    // Set fs pointer for all inodes
+    for (unsigned int i = 0; i < MAX_INODES; i++) {
+        inodes[i].fs = this;
+    }
+    
+    return true;
 }
 
-bool FileSystem::Format(SimpleDisk * _disk, unsigned int _size) { // static!
+bool FileSystem::Format(SimpleDisk * _disk, unsigned int _size) {
     Console::puts("formatting disk\n");
-    /* Here you populate the disk with an initialized (probably empty) inode list
-       and a free list. Make sure that blocks used for the inodes and for the free list
-       are marked as used, otherwise they may get overwritten. */
-    Console::puts("FUNCTION NOT IMPLEMENTED\n");
-    assert(false);
+    unsigned int num_blocks = _size / SimpleDisk::BLOCK_SIZE;
+    Inode* temp_inodes = new Inode[MAX_INODES];
+    unsigned char* temp_free_blocks = new unsigned char[num_blocks];
+    
+    // Initialize all inodes
+    for (unsigned int i = 0; i < MAX_INODES; i++) {
+        temp_inodes[i].id = 0;
+        temp_inodes[i].block_numbers_block = 0;
+        temp_inodes[i].num_blocks = 0;
+        temp_inodes[i].file_length = 0;
+        temp_inodes[i].fs = nullptr;
+    }
+    
+    // Initialize free blocks: 0 and 1 are used, rest are free
+    for (unsigned int i = 0; i < num_blocks; i++) {
+        temp_free_blocks[i] = (i < 2) ? 1 : 0;
+    }
+    
+    // Write to disk
+    _disk -> write(0, (unsigned char*)temp_inodes);
+    _disk -> write(1, temp_free_blocks);
+    
+    delete[] temp_inodes;
+    delete[] temp_free_blocks;
+    return true;
 }
 
 Inode * FileSystem::LookupFile(int _file_id) {
     Console::puts("looking up file with id = "); Console::puti(_file_id); Console::puts("\n");
-    /* Here you go through the inode list to find the file. */
-    Console::puts("FUNCTION NOT IMPLEMENTED\n");
-    assert(false);
+    for (unsigned int i = 0; i < MAX_INODES; i++) {
+        if (inodes[i].id == _file_id) {
+            return &inodes[i];
+        }
+    }
+    return nullptr;
 }
 
 bool FileSystem::CreateFile(int _file_id) {
     Console::puts("creating file with id:"); Console::puti(_file_id); Console::puts("\n");
-    /* Here you check if the file exists already. If so, throw an error.
-       Then get yourself a free inode and initialize all the data needed for the
-       new file. After this function there will be a new file on disk. */
-    Console::puts("FUNCTION NOT IMPLEMENTED\n");
-    assert(false);
+    if (LookupFile(_file_id) != nullptr) {
+        return false;
+    }
+    
+    short inode_idx = GetFreeInode();
+    if (inode_idx == -1) {
+        return false;
+    }
+    
+    // Allocate a block to store data block numbers
+    int block_nums_block = GetFreeBlock();
+    if (block_nums_block == -1) {
+        return false;
+    }
+    
+    // Initialize the block numbers block to zeros
+    unsigned int* block_nums = new unsigned int[SimpleDisk::BLOCK_SIZE / sizeof(unsigned int)];
+    for (unsigned int i = 0; i < SimpleDisk::BLOCK_SIZE / sizeof(unsigned int); i++) {
+        block_nums[i] = 0;
+    }
+    disk -> write(block_nums_block, (unsigned char*)block_nums);
+    delete[] block_nums;
+    
+    inodes[inode_idx].id = _file_id;
+    inodes[inode_idx].block_numbers_block = block_nums_block;
+    inodes[inode_idx].num_blocks = 0;
+    inodes[inode_idx].file_length = 0;
+    inodes[inode_idx].fs = this;
+    
+    free_blocks[block_nums_block] = 1;
+    
+    SaveInodes();
+    SaveFreeList();
+    return true;
 }
 
 bool FileSystem::DeleteFile(int _file_id) {
     Console::puts("deleting file with id:"); Console::puti(_file_id); Console::puts("\n");
-    /* First, check if the file exists. If not, throw an error. 
-       Then free all blocks that belong to the file and delete/invalidate 
-       (depending on your implementation of the inode list) the inode. */
-    Console::puts("FUNCTION NOT IMPLEMENTED\n");
-    assert(false);
+    Inode* inode = LookupFile(_file_id);
+    if (inode == nullptr) {
+        return false;
+    }
+    
+    // Free all data blocks
+    if (inode -> block_numbers_block != 0) {
+        unsigned int* block_nums = new unsigned int[SimpleDisk::BLOCK_SIZE / sizeof(unsigned int)];
+        disk -> read(inode -> block_numbers_block, (unsigned char*)block_nums);
+        
+        for (unsigned int i = 0; i < inode -> num_blocks; i++) {
+            if (block_nums[i] != 0) {
+                free_blocks[block_nums[i]] = 0;
+            }
+        }
+        
+        // Free the block numbers block
+        free_blocks[inode -> block_numbers_block] = 0;
+        delete[] block_nums;
+    }
+    
+    inode -> id = 0;
+    inode -> block_numbers_block = 0;
+    inode -> num_blocks = 0;
+    inode -> file_length = 0;
+    SaveInodes();
+    SaveFreeList();
+    return true;
+}
+
+short FileSystem::GetFreeInode() {
+    for (unsigned int i = 0; i < MAX_INODES; i++) {
+        if (inodes[i].id == 0) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int FileSystem::GetFreeBlock() {
+    unsigned int num_blocks = size / SimpleDisk::BLOCK_SIZE;
+    for (unsigned int i = 2; i < num_blocks; i++) {
+        if (free_blocks[i] == 0) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int FileSystem::GetFreeBlocks(unsigned int count, unsigned int* blocks) {
+    unsigned int num_blocks = size / SimpleDisk::BLOCK_SIZE;
+    unsigned int found = 0;
+    for (unsigned int i = 2; i < num_blocks && found < count; i++) {
+        if (free_blocks[i] == 0) {
+            blocks[found++] = i;
+        }
+    }
+    return (found == count) ? found : -1;
+}
+
+void FileSystem::SaveInodes() {
+    if (disk != nullptr && inodes != nullptr) {
+        disk -> write(0, (unsigned char*)inodes);
+    }
+}
+
+void FileSystem::SaveFreeList() {
+    if (disk != nullptr && free_blocks != nullptr) {
+        disk -> write(1, free_blocks);
+    }
 }
